fe4cfdcaf24d70aebbaaa000a99d19c0b02f9224
James Simpson <jamz@jamessimpson.co.uk>
2024-07-26 18:48:40 +0100

    migration
    migration file

---
+++ migrations/101_version_101.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Version_101 extends App_module_migration
+{
+    public function up()
+    {
+        // Nothing to do here
+    }
+}
